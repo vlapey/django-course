a7a7b75d3807291ba260cd002c9f25ddc77fed9f
Vladislav <vlad.lapey@gmail.com>
2023-11-14 03:12:38 +0300

    initial commit

---
+++ coolsite/women/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):  # HttpRequest
+    return HttpResponse("Страница приложения women.")
+
+
+def categories(request):
+    return HttpResponse("<h1>Статьи по категориям</h1>")
